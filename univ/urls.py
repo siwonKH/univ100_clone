a1660ba890402d7b1df3e67e4acd72251fa01dc8
siwonKH <kanghyunkim313@gmail.com>
2023-11-09 00:42:57 +0900

    added router

--- univ/urls.py
+++ univ/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from .views import Home, University
+from .views import Home, University, Question, Search, Post, QuestionDetail, Likes, Authenticate, Register
 
 
 urlpatterns = [
     path('', Home.as_view()),
     path('university', University.as_view()),
+    path('q', Question.as_view()),
+    path('search', Search.as_view()),
+    path('post', Post.as_view()),
+    path('q/<int:question_id>', QuestionDetail.as_view()),
+    path('likes', Likes.as_view()),
+    path('auth', Authenticate.as_view()),
+    path('register', Register.as_view()),
 ]
--- univ/views.py
+++ univ/views.py
@@ -1,5 +1,9 @@
 from django import views
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+
+from univ import utils
 
 
 class Home(views.View):
@@ -12,3 +16,72 @@ class University(views.View):
     @staticmethod
     def get(request):
         return render(request, 'university.html')
+
+
+class Question(views.View):
+    @staticmethod
+    def get(request):
+        return render(request, 'question.html')
+
+
+class Search(views.View):
+    @staticmethod
+    def get(request):
+        query = request.GET.get('q')
+
+        results = utils.search_university(query)
+
+        return render(request, 'search.html', {'results': results})
+
+
+class Post(views.View):
+    @staticmethod
+    def get(request):
+        return render(request, 'post.html')
+
+
+class QuestionDetail(views.View):
+    @staticmethod
+    def get(request, question_id):
+        return render(request, 'question_detail.html')
+
+
+class Likes(views.View):
+    @staticmethod
+    def get(request):
+        return render(request, 'likes.html')
+
+
+class Authenticate(views.View):
+    @staticmethod
+    def get(request):
+        return render(request, 'authenticate.html')
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'authenticate.html', {'error': 'Invalid username or password'})
+
+
+class Register(views.View):
+    @staticmethod
+    def get(request):
+        return render(request, 'register.html')
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if User.objects.filter(username=username).exists():
+            return render(request, 'register.html', {'error': 'Username already exists'})
+
+        User.objects.create_user(username=username, password=password)
+
+        return redirect('/auth')
